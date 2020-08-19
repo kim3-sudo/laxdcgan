ab933fe358bfa5ea48de40a8c1316290e9edc269
Sejin Kim <kim3@kenyon.edu>
2020-08-19 15:30:17 -0400

    Update and rename resize.py to 128resize.py

---
+++ 128resize.py
@@ -0,0 +1,12 @@
+import os
+import cv2
+
+src = "./data" #original
+dst = "./resizedData" # resized
+
+os.mkdir(dst)
+
+for each in os.listdir(src):
+    img = cv2.imread(os.path.join(src,each))
+    img = cv2.resize(img,(128,128))
+    cv2.imwrite(os.path.join(dst,each), img)
